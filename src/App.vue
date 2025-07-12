00ce896cc8ab797d16052f28148d9f5660d47c53
jeral-backup <jeral.rios@inacapmail.cl>
2025-07-11 21:00:43 -0400

    App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Gestor de Equipos Forestales</h1>
+
+    <ClimaZona />
+
+    <EquipoForm
+      :modoEdicion="modoEdicion"
+      :equipoEditar="equipoEnEdicion"
+      @guardar="guardarEquipo"
+    />
+
+    <ListaEquipos
+      :equipos="equipos"
+      @editar="editarEquipo"
+      @eliminar="eliminarEquipo"
+    />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import EquipoForm from './components/EquipoForm.vue'
+import ListaEquipos from './components/ListaEquipos.vue'
+import ClimaZona from './components/ClimaZona.vue'
+
+export default {
+  components: { EquipoForm, ListaEquipos,ClimaZona },
+  data() {
+    return {
+      equipos: [],
+      modoEdicion: false,
+      indexEnEdicion: null,
+      equipoEnEdicion: null
+    }
+  },
+  created() {
+    this.cargarDesdeLocalStorage()
+  },
+  methods: {
+    guardarEquipo(equipo) {
+      if (this.modoEdicion) {
+        this.equipos.splice(this.indexEnEdicion, 1, equipo)
+        this.modoEdicion = false
+        this.indexEnEdicion = null
+        this.equipoEnEdicion = null
+      } else {
+        this.equipos.push(equipo)
+      }
+      this.guardarEnLocalStorage()
+    },
+    editarEquipo(index) {
+      this.modoEdicion = true
+      this.indexEnEdicion = index
+      this.equipoEnEdicion = { ...this.equipos[index] }
+    },
+    eliminarEquipo(index) {
+      this.equipos.splice(index, 1)
+      this.guardarEnLocalStorage()
+    },
+    guardarEnLocalStorage() {
+      localStorage.setItem('equiposForestales', JSON.stringify(this.equipos))
+    },
+    cargarDesdeLocalStorage() {
+      const data = localStorage.getItem('equiposForestales')
+      if (data) {
+        this.equipos = JSON.parse(data)
+      }
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
