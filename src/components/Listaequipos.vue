db410c2940636696b27793854fc8d38b62077424
jeral-backup <jeral.rios@inacapmail.cl>
2025-07-11 21:01:31 -0400

    Update Listaequipos.vue

---
+++ src/components/Listaequipos.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for="(equipo, index) in equipos" :key="index">
+      {{ equipo.tipo }} - {{ equipo.estado }} - {{ equipo.fecha }}
+      <button @click="$emit('editar', index)">Editar</button>
+      <button @click="$emit('eliminar', index)">Eliminar</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['equipos']
+};
+</script>
