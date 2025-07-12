7e6342e84c936af04b24e275860a1cc183325364
jeral-backup <jeral.rios@inacapmail.cl>
2025-07-11 21:01:57 -0400

    ClimaZona.vue

---
+++ src/components/ClimaZona.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-if="clima">
+    <h2>Clima actual en Biobío 🌤️</h2>
+    <p>Temperatura: {{ clima.temperature }}°C</p>
+    <p>Viento: {{ clima.windspeed }} km/h</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clima: null
+    }
+  },
+  mounted() {
+    fetch('https://api.open-meteo.com/v1/forecast?latitude=-37.5&longitude=-72.5&current_weather=true')
+      .then(response => response.json())
+      .then(data => {
+        this.clima = data.current_weather
+      })
+      .catch(error => console.error('Error al cargar el clima:', error))
+  }
+}
+</script>
